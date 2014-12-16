1bdc64a33523a72ba134551160ade299c6d72520
thoffmann <toni.hoffmann@arthrex.de>
2014-12-16 16:48:44 +0100

    unsharkofy and localization

--- gif.bundle/plugin.py
+++ gif.bundle/plugin.py
@@ -6,10 +6,10 @@ def dark_mode():
 
 def results(parsed, original):
   import json
-  path = "sharkobot.html";
+  path = "gifbot.html";
   html = open(i18n.find_localized_path(path)).read().replace("var nullValue = null;", "var nullValue = \"{0}\";").replace('{0}', parsed['~query']).replace("light-mode", "dark-mode" if dark_mode() else "light-mode")
   return {
-    "title": u"\"{0}\" GIFs (Press enter to copy)".format(parsed['~query']),
+    "title": i18n.localstr(u"\"{0}\" GIFs (Press cmd + c to copy link)").format(parsed['~query']),
     "html": html,
     "pass_result_of_output_function_as_first_run_arg": True,
     "run_args": ["abc"],
