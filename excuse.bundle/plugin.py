df48dc9e0498326fd2e54599e238659f762e18c7
thoffmann <toni.hoffmann@arthrex.de>
2014-12-23 08:31:30 +0100

    finished programming excuse plugin

--- excuse.bundle/plugin.py
+++ excuse.bundle/plugin.py
@@ -4,15 +4,10 @@ def results(parsed, original_query):
     return {
         "title": i18n.localstr("Excuse"),
         "run_args": ['http://www.programmerexcuses.com'],
-        "html": """
-        <script>
-        setTimeout(function() {
-            window.location = %s
-        }, 500);
-        </script>
-        """%(json.dumps('http://www.programmerexcuses.com')),
+        "html" : open(i18n.find_localized_path("excuse.html")).read(),
         "webview_user_agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53",
-        "webview_links_open_in_browser": True
+        "webview_links_open_in_browser": True,
+        "webview_transparent_background": True
     }
 
 def run(url):
